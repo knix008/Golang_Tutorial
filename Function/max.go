1495ce30274b91dcb453df269d6aa1db23c5be8b
Suho Kwon <suho.kwon@gmail.com>
2024-05-14 17:56:01 +0900

    Add more examples.

---
+++ Function/max.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+/* function returning the max between two numbers */
+func max(num1, num2 int) int {
+	/* local variable declaration */
+	var result int
+
+	if num1 > num2 {
+		result = num1
+	} else {
+		result = num2
+	}
+	return result
+}
+
+func main() {
+	fmt.Println("The maximu value : ", max(1, 2))
+}
