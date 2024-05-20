802844c95b54a5dab3e68dc54d9be428cd560862
Suho Kwon <suho.kwon@gmail.com>
2024-05-20 17:34:57 +0900

    Add more examples.

---
+++ Error/forceError.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"math"
+)
+
+func Sqrt(value float64) (float64, error) {
+	if value < 0 {
+		return 0, errors.New("Math: negative number passed to Sqrt")
+	}
+	return math.Sqrt(value), nil
+}
+
+func main() {
+	var result, error = Sqrt(-10.0)
+	if error != nil {
+		fmt.Println("The error message", error)
+	} else {
+		fmt.Println("The SQRT :", result)
+	}
+}
